749daeb9875fb2cd9e8d9ee3362dc4852088cfbf
UsagiB4 <binaryBunny@tutanota.com>
2021-08-05 12:37:43 +0600

    Run this

---
+++ commandfile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+pip install termcolor
+cp /usr/lib/python3/dist-packages/termcolor.py /usr/lib/python2.7/
+cd /usr/lib/python2.7
+echo "termcolor.py has been copied"
+echo "Problem Solved"
+
+
+
+
